625c47f7f42f8f28b1ae460dfc75bafe8f445277
Steve Jobs <peter@steves-mbp.nvstu.edu>
2017-03-27 18:21:11 -0400

    swagger added

--- src/test/java/com/example/PeopleGroupsApplicationTests.java
+++ src/test/java/com/example/PeopleGroupsApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PeopleGroupsApplicationTests {
 
+	//http://localhost:8080/user
+
 	@Test
 	public void contextLoads() {
 	}
